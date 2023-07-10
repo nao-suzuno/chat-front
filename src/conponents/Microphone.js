54480ce0ddff546d8846ea4172354606eabe0de3
nao-suzuno <nao.suzuno@fujielab.org>
2023-07-10 13:25:02 +0900

    fix

--- src/conponents/Microphone.js
+++ src/conponents/Microphone.js
@@ -1,8 +1,9 @@
-import React from 'react';
+import React,{ useState } from 'react';
 import SpeechRecognition, { useSpeechRecognition } from 'react-speech-recognition';
 
 function Microphone () {
     const { listening, transcript, resetTranscript } = useSpeechRecognition();
+    const [result, setResult] = useState("");
   
     const handleStartListening = () => {
       SpeechRecognition.startListening({continuous: true});
@@ -15,7 +16,20 @@ function Microphone () {
     };
   
     const sendTranscriptToAPI = (transcript) => {
-      console.log('Transcript:', transcript);
+      fetch("https://pi4c8iu2g5.execute-api.us-east-1.amazonaws.com/default/chat-server?text=" + transcript, {method: 'GET', headers: {'Content-Type': 'application/json'}, mode: 'cors'})
+      .then(res => {
+        if (res.ok) {
+          return res.json();
+        } else {
+          throw new Error('APIリクエストエラー: ' + res.status);
+        }          
+      })
+      .then(data => {
+        setResult(data.result);
+      })
+      .catch((e) => {
+        console.error('Failed to check authorization status', e);
+      })
       resetTranscript();
     };
   
@@ -26,7 +40,8 @@ function Microphone () {
         <button onClick={handleStopListening} disabled={!listening}>音声入力終了</button>
         <button onClick={resetTranscript}>リセット</button>
 
-        <p>{transcript}</p>
+        <p>音声認識:{transcript}</p>
+        <p>返答結果:{result}</p>
       </div>
     );
   };
