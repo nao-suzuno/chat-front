0315dac8f7bd48d6fc5ed2c2e9367d872c285a46
nao-suzuno <nao.suzuno@fujielab.org>
2023-07-13 17:34:23 +0900

    fix

--- src/conponents/Microphone.js
+++ src/conponents/Microphone.js
@@ -5,6 +5,8 @@ import './Microphone.css';
 function Microphone () {
     const { listening, transcript, resetTranscript } = useSpeechRecognition();
     const [result, setResult] = useState("");
+    const [text, setText] = useState("");
+    const [log,setLog] = useState([]);
   
     const handleStartListening = () => {
       SpeechRecognition.startListening({continuous: true});
@@ -13,24 +15,31 @@ function Microphone () {
 
     const handleStopListening = () => {
       SpeechRecognition.stopListening();
+      setLog([...log,`user:${transcript}`])
       setTimeout(sendTranscriptToAPI(transcript),5000);
     };
   
-    const sendTranscriptToAPI = (transcript) => {
-      fetch("https://pi4c8iu2g5.execute-api.us-east-1.amazonaws.com/default/chat-server?text=" + transcript, {method: 'GET', headers: {'Content-Type': 'application/json'}, mode: 'cors'})
+    const sendTranscriptToAPI = async(transcript) => {
+      await fetch("https://pi4c8iu2g5.execute-api.us-east-1.amazonaws.com/default/chat-server?text=" + transcript, {method: 'GET', headers: {'Content-Type': 'application/json'}, mode: 'cors'})
       .then(res => {
         if (res.ok) {
           return res.json();
         } else {
+          setText("Error");
           throw new Error('APIリクエストエラー: ' + res.status);
         }          
       })
       .then(data => {
+        setText("DONE!");
+        setLog([...log,`system:${data.result}`])
         setResult(data.result);
       })
       .catch((e) => {
+        setText("Error");
         console.error('Failed to check authorization status', e);
       })
+
+      setText("Loading");
       resetTranscript();
     };
   
@@ -44,6 +53,17 @@ function Microphone () {
         </div>
         <p>送信内容(音声認識):{transcript}</p>
         <p>返答結果:{result}</p>
+        <p>status:{text}</p>
+        <p>Log</p>
+        {
+          (function () {
+            const list = [];
+            for (const t of log) {
+              list.push(<li>{t}</li>);
+            }
+            return <ul>{list}</ul>;
+          }())
+        }
       </div>
     );
   };
