49dd118355d1b980dbc0dfa560338a4c2b86b0ee
NaoSuzuno <84945656+N7-n@users.noreply.github.com>
2023-09-20 23:09:19 +0900

    中間発表

--- src/App.js
+++ src/App.js
@@ -1,11 +1,68 @@
-import React from 'react';
-import Microphone from './conponents/Microphone';
+import React, { useState, useEffect, useMemo } from 'react';
+import SpeechRecognition, { useSpeechRecognition } from 'react-speech-recognition';
 
 
-function App() {
+const App = () => {
+  const [message, setMessage] = useState('');
+  const [sent, setSent] = useState(false);
+  const { transcript, resetTranscript, listening, interimTranscript } = useSpeechRecognition();
+
+  let socket = useMemo(() => new WebSocket('ws://localhost:8080'), []);
+
+  useEffect(() => {
+    if (transcript && !interimTranscript && listening) {
+      // 音声データをWebSocketを介して送信
+      socket.send(transcript);
+      resetTranscript();
+      setSent(true);
+    }
+  }, [transcript, listening, resetTranscript, socket, interimTranscript]);
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      if (!interimTranscript && sent) {
+        setSent(false);
+        console.log('入力完了');
+        socket.send('done');
+      }
+    }, 3000);
+
+    return () => {
+      clearTimeout(timer);
+    };
+  }, [interimTranscript, socket, sent]);
+
+  const startRecording = () => {
+    // 録音を開始
+    SpeechRecognition.startListening({ continuous: true });
+  };
+
+  socket.onmessage = (event) => {
+    setMessage(event.data);
+    SpeechRecognition.stopListening();
+    // テキストを音声で読み上げる
+    const utterance = new SpeechSynthesisUtterance(event.data);
+    window.speechSynthesis.speak(utterance); 
+    utterance.onend = () => {
+      SpeechRecognition.startListening({ continuous: true });
+    };  
+  };
+
+  const stopRecording = () => {
+    SpeechRecognition.stopListening();
+  };
+
   return (
     <div>
-      <Microphone/>
+      <button onClick={startRecording} disabled={listening}>
+        Start Recording
+      </button>
+      <button onClick={stopRecording} disabled={!listening}>
+        Stop Recording
+      </button>
+      <p>Microphone: {listening ? 'on' : 'off'}</p>
+      <p>sent:{sent ? 'on' : 'off'}</p>
+      <p>{message}</p>
     </div>
   );
 }
--- src/conponents/Microphone.js
+++ src/conponents/Microphone.js
@@ -1,67 +1,91 @@
-import React, { useState, useEffect } from 'react';
+// import React, { useState, useEffect } from 'react';
+
+// const Microphone = () => {
+//   const [isRecording, setIsRecording] = useState(false);
+//   const [socket, setSocket] = useState(null);
+//   const [mediaStream, setMediaStream] = useState(null);
+//   const [mediaRecorder, setMediaRecorder] = useState(null);
+
+//   useEffect(() => {
+//     const ws = new WebSocket('ws://localhost:8080/ws');
+//     setSocket(ws);
+
+//     return () => {
+//       ws.close();
+//     };
+//   }, []);
+
+//   const handleStartRecording = async () => {
+//     try {
+//       const stream = await navigator.mediaDevices.getUserMedia({ audio: true });
+
+//       const recorder = new MediaRecorder(stream);
+
+//       recorder.ondataavailable = (event) => {
+//         if (event.data.size > 0) {
+//           if (socket.readyState === WebSocket.OPEN) {
+//             socket.send(event.data);
+//           }
+//         }
+//       };
+
+//       // 録音を開始
+//       recorder.start();
+
+//       setIsRecording(true);
+//       setMediaStream(stream);
+//       setMediaRecorder(recorder);
+//     } catch (error) {
+//       console.error('Error accessing microphone:', error);
+//     }
+//   };
+
+//   const handleStopRecording = () => {
+//     if (mediaRecorder) {
+//       mediaRecorder.stop();
+//       mediaStream.getTracks().forEach((track) => track.stop());
+//       setIsRecording(false);
+//     }
+//   };
+
+//   return (
+//     <div className="App">
+//       <header className="App-header">
+//         <h1>WebSocket Mic Streaming</h1>
+//         {!isRecording ? (
+//           <button onClick={handleStartRecording}>Start Recording</button>
+//         ) : (
+//           <button onClick={handleStopRecording}>Stop Recording</button>
+//         )}
+//       </header>
+//     </div>
+//   );
+// };
+
+// export default Microphone;
+import React from 'react';
+import SpeechRecognition, { useSpeechRecognition } from 'react-speech-recognition';
 
 const Microphone = () => {
-  const [isRecording, setIsRecording] = useState(false);
-  const [socket, setSocket] = useState(null);
-  const [mediaStream, setMediaStream] = useState(null);
-  const [mediaRecorder, setMediaRecorder] = useState(null);
+  const {
+    transcript,
+    listening,
+    resetTranscript,
+    browserSupportsSpeechRecognition
+  } = useSpeechRecognition();
 
-  useEffect(() => {
-    const ws = new WebSocket('ws://localhost:8080/ws');
-    setSocket(ws);
-
-    return () => {
-      ws.close();
-    };
-  }, []);
-
-  const handleStartRecording = async () => {
-    try {
-      const stream = await navigator.mediaDevices.getUserMedia({ audio: true });
-
-      const recorder = new MediaRecorder(stream);
-      const chunks = [];
-
-      recorder.ondataavailable = (event) => {
-        if (event.data.size > 0) {
-          chunks.push(event.data);
-          if (socket.readyState === WebSocket.OPEN) {
-            socket.send(event.data);
-          }
-        }
-      };
-
-      // 録音を開始
-      recorder.start();
-
-      setIsRecording(true);
-      setMediaStream(stream);
-      setMediaRecorder(recorder);
-    } catch (error) {
-      console.error('Error accessing microphone:', error);
-    }
-  };
-
-  const handleStopRecording = () => {
-    if (mediaRecorder) {
-      mediaRecorder.stop();
-      mediaStream.getTracks().forEach((track) => track.stop());
-      setIsRecording(false);
-    }
-  };
+  if (!browserSupportsSpeechRecognition) {
+    return <span>Browser doesn't support speech recognition.</span>;
+  }
 
   return (
-    <div className="App">
-      <header className="App-header">
-        <h1>WebSocket Mic Streaming</h1>
-        {!isRecording ? (
-          <button onClick={handleStartRecording}>Start Recording</button>
-        ) : (
-          <button onClick={handleStopRecording}>Stop Recording</button>
-        )}
-      </header>
+    <div>
+      <p>Microphone: {listening ? 'on' : 'off'}</p>
+      <button onClick={SpeechRecognition.startListening}>Start</button>
+      <button onClick={SpeechRecognition.stopListening}>Stop</button>
+      <button onClick={resetTranscript}>Reset</button>
+      <p>{transcript}</p>
     </div>
   );
 };
-
 export default Microphone;
